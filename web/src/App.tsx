ca521dfa1db69e20322e4ff2471a149fc77aae7f
João Paulo Godinho <j_paulo_g@hotmail.com>
2023-01-19 23:11:31 -0300

    commit finalizando aula 2 e 3

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from './components/header';
+import { SummaryTable } from './components/SummaryTable';
+import './styles/global.css';
+ 
+
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w5xl px-6 flex flex-col gap-16" >
+      <Header />
+      <SummaryTable />
+      </div> 
+    </div>
+  )
+}
+ 
